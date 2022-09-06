b810380f2611b36dd9b81d2aacbfd71d80f3e950
Emily Bradfield <emily-bradfield@outlook.com>
2022-09-06 11:40:17 +0100

    simple structure created in newreleases.jsx

--- CinemaApp/frontend/src/components/newreleases.jsx
+++ CinemaApp/frontend/src/components/newreleases.jsx
@@ -1,5 +1,28 @@
-const NewReleases = () =>{
-
+const NewReleases = () => {
+    return (
+        <div className="center">
+            <h1> Out Now! </h1>
+            <div>
+                <nav className="navbar poster-nav">
+                    <ul className="navbar">
+                        <li className="nav-item nav-link">
+                            <Link to="/new-releases">placeholder</Link>
+                        </li>
+                        <li className="nav-item nav-link">
+                            <Link to="/new-releases">placeholder</Link>
+                        </li>
+                        <li className="nav-item nav-link">
+                            <Link to="/new-releases">placeholder</Link>
+                        </li>
+                        <li className="nav-item nav-link">
+                            <Link to="/new-releases">placeholder</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Outlet />
+            </div>
+        </div>
+    );
 }
 
 export default NewReleases;
