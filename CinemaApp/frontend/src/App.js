0dc93bc2eecf522ecbe456afb200f9205a6ab132
shahmeen-shaikh <shahmeen_shaikh@outlook.com>
2022-09-06 09:23:27 +0100

    home page complete

--- CinemaApp/frontend/src/App.js
+++ CinemaApp/frontend/src/App.js
@@ -1,14 +1,17 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
-import Navbar from './components/navbar';
+
+import Navbar from './components/navbar.jsx';
+import Home from './components/home.jsx';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+
           <Route exact path="/" element={Navbar}>
-            <Route index element={<p>Homepage Component</p>}/>
+            <Route index element={<Home/>}/>
             <Route path="/path-to-page" element={<p>Another Component</p>}/>
           </Route>
         </Routes>
---
+++ CinemaApp/frontend/src/components/home.jsx
@@ -0,0 +1,39 @@
+//import demo from "../assets/moo-nsters-inc.png";
+const Home = () => {
+
+   return(
+    <>
+        <div className="main-text">
+            <div>
+               <h1 className="title">QA Cinemas</h1>
+               <hr/>
+               <p className="text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rerum quae sed cumque itaque nostrum velit non perferendis magni quibusdam neque beatae maiores quidem, repellendus et commodi inventore placeat nisi?</p>
+            </div>
+            <div className="col">
+                <div className="card-img card1">
+                    <h4>Enjoy</h4>
+                    <p>Relax yourself to the fullest</p>
+                </div>
+                <div className="card-img card2">
+                    <h4>Escape</h4>
+                    <p>Get lost in the excitment</p>
+                </div>
+                <div className="card-img card3">
+                    <h4>Explore</h4>
+                    <p>Discover riveting excitment</p>
+                </div>
+                <div className="card-img card4">
+                    <h4>Experience</h4>
+                    <p>Experience new adventures</p>
+                </div>
+
+            </div>
+        </div>
+    
+    
+    </>
+   )
+
+} 
+
+export default Home;
