bc275dc0ae3ef2f99e46c494f3819a8839a422c9
shahmeen-shaikh <shahmeen_shaikh@outlook.com>
2022-09-06 17:02:15 +0100

    home page adjusted

--- CinemaApp/frontend/src/components/home.jsx
+++ CinemaApp/frontend/src/components/home.jsx
@@ -5,9 +5,9 @@ const Home = () => {
     <>
         <div className="main-text">
             <div className="main-text">
-               <h1 className="title">QA Cinemas</h1>
-               <hr/>
-               <p className="text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rerum quae sed cumque itaque nostrum velit non perferendis magni quibusdam neque beatae maiores quidem, repellendus et commodi inventore placeat nisi?</p>
+               <h1 className="title">QA Cinemas<hr/></h1>
+               
+               <p className="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo voluptas esse aut, tenetur ducimus aspernatur impedit enim exercitationem optio aliquid dignissimos. Magni voluptatibus quod veritatis! Quod ipsam dolor assumenda similique.</p>
             </div>
             <div className="col">
                 <div className="card-img card1">
@@ -20,7 +20,7 @@ const Home = () => {
                 </div>
                 <div className="card-img card3">
                     <h4>Explore</h4>
-                    <p>Discover riveting excitment</p>
+                    <p>Discover riveting stories</p>
                 </div>
                 <div className="card-img card4">
                     <h4>Experience</h4>
@@ -29,6 +29,50 @@ const Home = () => {
 
             </div>
         </div>
+
+  {/* <div>
+    <h2 className="soon">Coming Soon...</h2>
+   </div>  */}
+
+
+{/* <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://images.pexels.com/photos/10188205/pexels-photo-10188205.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.pexels.com/photos/1225236/pexels-photo-1225236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.pexels.com/photos/315191/pexels-photo-315191.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div> */}
     
     
     </>
