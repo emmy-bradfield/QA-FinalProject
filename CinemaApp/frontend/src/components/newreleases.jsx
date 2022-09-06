5a316e42c81a5cd9515ddc1fa12af77002043d8d
shahmeen-shaikh <shahmeen_shaikh@outlook.com>
2022-09-06 12:11:50 +0100

    navbar fixed

---
+++ CinemaApp/frontend/src/components/newreleases.jsx
@@ -0,0 +1,30 @@
+import { Link, Outlet } from "react-router-dom";
+
+const NewReleases = () => {
+    return (
+        <div className="center">
+            <h1> Out Now! </h1>
+            <div>
+                <nav className="navbar poster-nav">
+                    <ul className="navbar">
+                        <li className="nav-item nav-link">
+                            <Link to="/new-releases">placeholder</Link>
+                        </li>
+                        <li className="nav-item nav-link">
+                            <Link to="/new-releases">placeholder</Link>
+                        </li>
+                        <li className="nav-item nav-link">
+                            <Link to="/new-releases">placeholder</Link>
+                        </li>
+                        <li className="nav-item nav-link">
+                            <Link to="/new-releases">placeholder</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Outlet />
+            </div>
+        </div>
+    );
+}
+
+export default NewReleases;
