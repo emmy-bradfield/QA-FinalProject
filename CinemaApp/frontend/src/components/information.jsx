2beabe9fde3e94d27fa18d65268e318148d1cb31
Emily Bradfield <emily-bradfield@outlook.com>
2022-09-07 11:32:37 +0100

    screens page completed

--- CinemaApp/frontend/src/components/information.jsx
+++ CinemaApp/frontend/src/components/information.jsx
@@ -1,22 +1,27 @@
-import {Link, Outlet} from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
-const Information = () =>{
-    return(
+const Information = () => {
+    return (
         <main className="center lift3">
-            <h1 className="light-text"> What to Expect </h1>
-            <nav className='navbar center'>
-                <ul className="main-nav">
-                    <li className="navi">
-                        <Link to="/information/screens">Screens</Link>
-                    </li>
-                    <li className="navi">
-                        <Link to="/information/directions">Find Us</Link>
-                    </li>
-                    <li className="navi">
-                        <Link to="/information/nearby">Places to go</Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className="minor-header">
+                <Link to="/information"><h1 className="light-text"> What to Expect </h1></Link>
+                <hr />
+                <nav className="secondary-nav">
+                    <ul className="secondary-nav">
+                        <li className="navi2">
+                            <Link to="/information/screens">Screens</Link>
+                        </li>
+                        <li className="navi2">
+                            <Link to="/information/directions">Find Us</Link>
+                        </li>
+                        <li className="navi2">
+                            <Link to="/information/nearby">Places to go</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <br/>
+            <Outlet />
         </main>
     );
 }
--- CinemaApp/frontend/src/components/screens.jsx
+++ CinemaApp/frontend/src/components/screens.jsx
@@ -1,7 +1,65 @@
-const Screens = () =>{
-    return(
-        <main>
+import StandardSeats from '../assets/standard-seats.png';
+import StandardScreen from '../assets/standard-screen.jpg';
+import DeluxeSeats from '../assets/deluxe-seats.png';
+import DeluxeScreen from '../assets/deluxe-screen.jpg';
+
+const Screens = () => {
+    return (
+        <main className="center">
             <h1> Our Screens </h1>
+            <div className="card-subtle">
+                <h5 className="card-inner-text">Standard Screen</h5>
+                <p className="card-inner-text">Our standard screen comes with all you could need for a mind-blowing,
+                    udderly unbeefable cinematic experience! Our recent refurbishment means we are able to offer you
+                    the highest-quality and most luxurious moo-vie experience. Don't believe us? Take a look for
+                    yourself!</p>
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={StandardSeats} class="d-block w-100" alt="..." />
+                        </div>
+                        <div class="carousel-item">
+                            <img src={StandardScreen} class="d-block w-100" alt="..." />
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+                <hr />
+
+                <h5 className="card-inner-text">Deluxe "Directors Box" Seats</h5>
+                <p className="card-inner-text">Want a truly immersive experience? We offer deluxe Director's Box seats for many
+                of our showings. Located on the first floor of each cinema room. Housing a limited number of seats, with 
+                fully emoosive surround sound speakers, additional legroom, wider and comfier seats, and an all-round mind 
+                blowing experience! Oh, and did we forget to mention? The directors box boasts a ultra-wide HD 8k screen 
+                - incredable! </p>
+                <div id="carouselExampleControls2" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={DeluxeSeats} class="d-block w-100" alt="..." />
+                        </div>
+                        <div class="carousel-item">
+                            <img src={DeluxeScreen} class="d-block w-100" alt="..." />
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+            </div>
         </main>
     );
 };
