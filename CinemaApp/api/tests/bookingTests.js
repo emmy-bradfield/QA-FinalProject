6b6bf3d9930391cd8210073035832768743c337c
Emily Bradfield <emily-bradfield@outlook.com>
2022-09-13 18:19:19 +0100

    bookingTests completed

--- CinemaApp/api/tests/bookingTests.js
+++ CinemaApp/api/tests/bookingTests.js
@@ -6,7 +6,52 @@ const expect = chai.expect;
 const Booking = require("../Models/bookingModel");
 const {response} = require('express');
 
-const readBooking = {
+const EDIT = {
+    __v: 0,
+    _id: "EDIT",
+    firstName: "John",
+    lastName: "Doe",
+    movie: "Cowsablanca",
+    day: "16-Sep",
+    time: "12:00",
+    price: "10",
+    tickets: [
+        {
+            noOfAdult: "3",
+            noOfChild: "1",
+            noOfConcession: "0"
+        }
+    ],
+    payment: [
+        {
+            cardName: "Mr John Doe",
+            cardNumber: "4921889745651322",
+            cardDate: "0526",
+            cardCVC: "707"
+        }
+    ]
+}
+
+const PAY = {
+    __v: 0,
+    _id: "PAY",
+    firstName: "John",
+    lastName: "Doe",
+    movie: "Cowsablanca",
+    day: "16-Sep",
+    time: "12:00",
+    price: "10",
+    tickets: [
+        {
+            noOfAdult: "3",
+            noOfChild: "1",
+            noOfConcession: "0"
+        }
+    ],
+    payment: [{}]
+}
+
+const TEST = {
     __v: 0,
     _id: "TEST",
     firstName: "John",
@@ -32,7 +77,7 @@ const readBooking = {
     ]
 };
 
-const createdBooking = {
+const CREATED = {
     __v: 0,
     _id: "CREATED",
     day: "15-Sep",
@@ -52,7 +97,7 @@ const createdBooking = {
     time: "17:00"
 };
 
-const checkoutBooking = {
+const exPAY = {
     __v: 0,
     _id: "PAY",
     firstName: "John",
@@ -79,7 +124,7 @@ const checkoutBooking = {
 
 }
 
-const ticketBooking = {
+const exEDIT = {
     _id: "EDIT",
     firstName: "John",
     lastName: "Doe",
@@ -107,39 +152,46 @@ const ticketBooking = {
 }
 
 describe("CRUD testing", () => {
+    beforeEach(() => {
+        Booking.deleteMany({})
+        Booking.create(TEST)
+        Booking.create(PAY)
+        Booking.create(EDIT)
+    });
 
     it("Should create a movie", function () {
-        chai.request(server).post("/bookings/post").send(createdBooking).end((err, res) => {
+        chai.request(server).post("/bookings/post").send(CREATED).end((err, res) => {
             chai.expect(err).to.be.null;
-            chai.expect(res.body).to.eql(createdBooking);
+            chai.expect(res.body).to.eql(CREATED);
         });
     });
 
     it("Should find all movies", function () {
         chai.request(server).get("/bookings/getAll").end((err, res) => {
             chai.expect(err).to.be.null;
-            chai.expect(res.body).to.have.lengthOf(4);
-            chai.expect(res.body[0]).to.eql(readBooking);
+            chai.expect(res.body).to.have.lengthOf(3);
+            chai.expect(res.body[0]).to.eql(TEST);
         });
     });
 
     it("Should find a specific movie", function () {
-        chai.request(server).get("/bookings/get/TEST").end(function (err, res) {
+        chai.request(server).get("/bookings/get/TEST").query("TEST").end(function (err, res) {
             expect(err).to.be.null;
-            expect(res.body).to.eql(readBooking);
+            expect(res.body).to.eql(TEST);
         })
     });
 
     it("Should add payment details to a booking", function () {
         chai.request(server).post("/bookings/checkout/PAY").query("PAY").send({cardName: 'Mr John Doe', cardNumber: '4921889745651322', cardDate: '0526', cardCVC: '707'}).end((err, res) => {
             chai.expect(err).to.be.null;
-            expect(res.body).to.eql(checkoutBooking);
+            expect(res.body).to.eql(exPAY);
+
         })
     });
 
     it("Should update tickets for a booking", function () {
-        chai.request(server).post("/bookings/update/EDIT").query("EDIT").send({noOfAdult: "2", noOfChild: "0", noOfConcession: "1"}).end((err, res) => {
-            expect(res.body).to.eql(ticketBooking);
+        chai.request(server).post("/bookings/update/EDIT").query("EDIT").send(exEDIT).end((err, res) => {
+            expect(res.body).to.eql(exEDIT);
         });
     });
 
