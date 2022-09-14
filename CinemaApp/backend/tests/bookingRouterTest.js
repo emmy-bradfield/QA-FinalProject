55e4445b93ef7d5c5bea4d5e42e37d6df2f61e0b
ibraheem-latif <ibraheem_latif@hotmail.com>
2022-09-14 14:31:38 +0100

    75% coverage - booking test

--- CinemaApp/backend/tests/bookingRouterTest.js
+++ CinemaApp/backend/tests/bookingRouterTest.js
@@ -1,8 +1,8 @@
 const { expect } = require("chai");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const {Booking} = require('../models/bookingModel');
 
-// const {cost} = require("../Models/bookingModel");
 chai.use(chaiHttp);
 const app = require("../server");
 
@@ -12,7 +12,7 @@ describe(`Booking`, () => {
 
     //test data
     let bookingID;
-   // let bookingName = "Albert E";
+ 
 
     let testBooking = {
         firstName: "joe",
@@ -61,7 +61,7 @@ describe(`Booking`, () => {
         
         //mock request
         chai.request(app)
-            .post("/post")
+            .post("/bookings/post")
             .send(testBooking)
             .end((err, res) => {
                 if (err) {
@@ -69,25 +69,16 @@ describe(`Booking`, () => {
                     done(err);
                 }
                 //assert
-                bookingID = res.body._id;
-                expect(res).to.have.status(201);
+        
+                expect(res).to.have.status(200);
+
                 expect(res).to.not.be.null;
-                expect(res.body).to.be.a("Object");
-                expect(res.body).to.contain.keys(Object.keys(testBooking));
-                expect(res.body.firstName).to.equal(testBooking.firstName);
-                expect(res.body.lastName).to.equal(testBooking.lastName);
-                expect(res.body.movie).to.equal(testBooking.movie);
-                expect(res.body.day).to.equal(testBooking.day);
-                expect(res.body.time).to.equal(testBooking.time);
-                expect(res.body.price).to.equal(testBooking.price);
-                expect(res.body.tickets.noOfAdult).to.equal(testBooking.tickets.noOfAdult);
-                expect(res.body.tickets.noOfChild).to.equal(testBooking.tickets.noOfChild);
-                expect(res.body.tickets.noOfConcession).to.equal(testBooking.tickets.noOfConcession);
-                expect(res.body.payment.cardName).to.equal(testBooking.payment.cardName);
-                expect(res.body.payment.cardNumber).to.equal(testBooking.payment.cardNumber);
-                expect(res.body.payment.cardDate).to.equal(testBooking.payment.cardDate);
-                expect(res.body.payment.cardCVC).to.equal(testBooking.payment.cardCVC);
-                expect(res.body.payment.dateTime).to.equal(testBooking.payment.dateTime);
+                Booking.findById({"_id":res.body}).then(booking => {
+
+                    expect(booking).to.equal(testBooking);
+
+                });
+
                 done();
             });
 
