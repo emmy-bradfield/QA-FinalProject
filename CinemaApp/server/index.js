68f90d29fc0f0162b7ab5ed91e73673f97161b34
Emily Bradfield <emily-bradfield@outlook.com>
2022-09-14 10:50:43 +0100

    app built

---
+++ CinemaApp/server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 4000;
+const path = requrie("path");
+app.use(express.json());
+app.use(cors());
+
+const mongoose = require("mongoose");
+const bookingRoute = require("./routes/bookingRouter");
+app.use("/bookings", bookingRoute);
+const emailRoute = require("./routes/emailRouter");
+app.use("/emails", emailRoute);
+const forumRoute = require("./routes/forumRouter");
+app.use("/forum", forumRoute);
+const router = express.Router();
+
+
+
+mongoose
+     .connect("mongodb+srv://team-lemon:cheese.please1@qacinema.zx2mivg.mongodb.net/cinema", {useNewUrlParser: true})
+     .then(() => console.log("MongoDB has been connected"))
+     .catch((err) => console.log(err));
+
+const connection = mongoose.connection;
+
+connection.once("open", function () {
+    console.log("Connection with MongoDB was successful");
+});
+
+app.use(express.static(path.resolve(__dirname, "../client/build"));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+})
+
+const server = app.listen(PORT, function () {
+    console.log("Server is running on Port: " + PORT);
+});
