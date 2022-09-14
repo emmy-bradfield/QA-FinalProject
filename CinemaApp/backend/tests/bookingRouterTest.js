dcbe3274fa142213976df3bf94c639884b56b85b
ibraheem-latif <ibraheem_latif@hotmail.com>
2022-09-14 17:17:36 +0100

    delete function tested + read function tweaked

--- CinemaApp/backend/tests/bookingRouterTest.js
+++ CinemaApp/backend/tests/bookingRouterTest.js
@@ -9,11 +9,6 @@ const app = require("../server");
 
 describe(`Booking`, () => {
 
-
-    //test data
-   // let bookingID;
- 
-
     let testBooking = {
         firstName: "joe",
         lastName: "M",
@@ -35,31 +30,29 @@ describe(`Booking`, () => {
         }
     };
 
-    // const updateBooking = {
-    //     firstName: "Joe",
-    //     lastName: "Mama",
-    //     movie: "cowsablanca",
-    //     day: "Tuesday",
-    //     time: "20:00",
-    //     price: "10",
-    //     tickets: {
-    //         noOfAdult: 1,
-    //         noOfChild: 0,
-    //         noOfConcession: 0
-    //     },
-    //     payment: {
-    //         cardName: "teri",
-    //         cardNumber: "42069",
-    //         cardDate: "12/27",
-    //         cardCVC: "123",
-    //         dateTime: "12/12/12"
-    //     }
-    // }
+    const newBooking ={
+        firstName: "Joe",
+        lastName: "Mama",
+        movie: "Cowsablanca",
+        day: "17-Sep",
+        time: "20:00",
+        price: "100",
+        tickets: {
+            noOfAdult: "10",
+            noOfChild: "0",
+            noOfConcession: "0"
+        },
+        payment: {
+            cardName: "Mr J. Mama",
+            cardNumber: "4200696942006969",
+            cardDate: "1227",
+            cardCVC: "123",
+            dateTime: "2022-09-15"
+        }
+    };
 
+//  CREATE
     it(`Should return posted booking when /booking is posted`, (done) => {
-        
-        
-        //mock request
         chai.request(app)
             .post("/bookings/post")
             .send(testBooking)
@@ -83,6 +76,50 @@ describe(`Booking`, () => {
             });
 
     });
+
+    // READ
+    it('Should return all bookings, including the one posted', (done) => {
+        chai.request(app)
+        .get("/bookings/getAll")
+        .send(newBooking)
+        .end((err, res)=> {
+            if(err) {
+                console.log(`Something went wrong: ${err}`);
+                done(err);
+            }
+            expect(res).to.have.status(200);
+            expect(res).to.not.be.null;
+            Booking.find().then(bookings => {
+                expect(bookings).not.to.have.lengthOf(0);
+                expect(bookings).to.include(newBooking);
+            });
+            done();
+        });
+    });
+
+    // DELETE
+
+    it('Should delete a booking', (done) => {
+        chai.request(app)
+        .delete(`/bookings/delete/${"_id"}`)
+        .send(testBooking)
+        .end((err, res)=> {
+            if(err) {
+                console.log(`Something went wrong: ${err}`);
+                done(err);
+            }
+            expect(res).to.have.status(200);
+
+            expect(res).to.not.be.null;
+            Booking.findByIdAndDelete({"_id":res.body}).then(bookings => {
+                expect(bookings).to.have.lengthOf(0);
+            });
+            done();
+        });
+    });
+
+
+
 });
 
 
